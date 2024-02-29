2818e54b051d17f93c10ae656830580d4f1f8a15
Gokulmurugan1997 <Gokulanshvi@gmail.com>
2024-02-29 11:29:48 +0530

    UPDATE ENV

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import AppRoutes from "./src/routes/index.js";
 import cors from "cors";
-const PORT = 9000
+const PORT = process.env.PORT || 9000
 const app = express()
 app.use(cors())
 app.use(express.json())
